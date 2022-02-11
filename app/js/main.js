ebfbdf1cd6bc46acf9ae10eaa373c2fea3ee618e
Denys Vovk <denysvovk@MacBook-Air-Denys.local>
2022-02-11 14:55:30 +0200

    minor fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
-$('.faq__btn').on('click', function() {
-    $(this.nextElementSibling.children[1]).slideToggle();
-    $(this).toggleClass('active');
-  });
+// $('.faq__btn').on('click', function() {
+//     $(this.nextElementSibling.children[1]).slideToggle();
+//     $(this).toggleClass('active');
+//   });
+
+$('.faq__btn').on('click', function () {
+  $(this.parentElement.parentElement.children[1]).slideToggle();
+  $(this).toggleClass('active');
+});
